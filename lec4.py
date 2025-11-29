c23a91ab67542a0873de608ba448d4669b8b5d66
ENG-M0H-S <ayaeng058@gmail.com>
2025-11-30 00:59:32 +0300

    ali

--- lec4.py
+++ lec4.py
@@ -19,6 +19,18 @@ class Student:
     def info(self):
         print(f"{self.name} - {self.id} - {self.gpa}")
 
-s1 = Student("Ali", 123, 3.5)
-s1.promote()
-s1.info()
+# s1 = Student("Ali", 123, 3.5)
+# s1.promote()
+# s1.info()
+
+class Car:
+    def __init__(self, model, year, mileage):
+        self.model = model
+        self.year = year
+        self.mileage = mileage
+
+    def drive(self, distance):
+        self.mileage += distance
+
+    def info(self):
+        print(f"{self.model} - {self.year} - {self.mileage} km")
